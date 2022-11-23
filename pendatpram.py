a5e1c2b8c711c03e8a7a1a43fedfb1bbeca90687
pramdf042 <84129088+pramdf042@users.noreply.github.com>
2022-11-24 01:29:54 +0700

    Update pendatpram.py

--- pendatpram.py
+++ pendatpram.py
@@ -1,134 +1,193 @@
 import streamlit as st
-import numpy as np
 import pandas as pd
+import numpy as np
+from sklearn import preprocessing
 from sklearn.preprocessing import MinMaxScaler
-from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
-from collections import OrderedDict
-from sklearn.naive_bayes import GaussianNB
-from sklearn.metrics import confusion_matrix, accuracy_score, recall_score, precision_score, f1_score
 from sklearn.neighbors import KNeighborsClassifier
+from numpy import array
+from sklearn import tree
+from sklearn.naive_bayes import GaussianNB
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import confusion_matrix, accuracy_score, recall_score, precision_score, f1_score
 from sklearn.tree import DecisionTreeClassifier
+from collections import OrderedDict
+from sklearn.neighbors import KNeighborsClassifier
 from sklearn.ensemble import BaggingClassifier
 from sklearn.datasets import make_classification
 from sklearn.svm import SVC
+import altair as alt
+from sklearn.utils.validation import joblib
+
 
 st.write(""" 
 # TUGAS PENDAT
 """)
 
-importdata, preprocessing, modelling, evaluasi, implementasi = st.tabs(["Import Data", "Preprocessing", "Modelling", "Evaluasi", "Implementasi"])
+description, importdata, preprocessing, modelling, implementation = st.tabs(["Description", "Import Data", "Preprocessing", "Modelling", "Implementation"])
+
+
 
 with importdata:
-    st.write("Import Data")
-    data = pd.read_csv("https://raw.githubusercontent.com/pramdf042/datamining/main/credit_score.csv")
-    st.dataframe(data)
+    st.write("""# Upload File""")
+    uploaded_files = st.file_uploader("Upload file CSV", accept_multiple_files=True)
+    for uploaded_file in uploaded_files:
+        df = pd.read_csv(uploaded_file)
+        st.write("Nama File Anda = ", uploaded_file.name)
+        st.dataframe(df)
+
 
 with preprocessing:
-    data.head()
+    st.write("""# Preprocessing""")
+    df = pd.read_csv("https://raw.githubusercontent.com/pramdf042/datamining/main/water_potability.csv")
+    df['Sulfate'] = df['Sulfate'].fillna(value=df['Sulfate'].median())
+    df['ph'] = df['ph'].fillna(value=df['ph'].median())
+    df['Trihalomethanes'] = df['Trihalomethanes'].fillna(value=df['Trihalomethanes'].median())
+    df
+    df[["ph", "Hardness", "Solids", "Chloramines", "Sulfate", "Conductivity", "Organic_carbon", "Trihalomethanes", "Turbidity"]].agg(['min','max'])
+    df.Potability.value_counts()
+    X = df.drop(columns="Potability")
+    y = df["Potability"]
+    "### Normalize data transformasi"
+    X
 
-    X = data.drop(columns=["risk_rating"])
+    X.shape, y.shape
 
-    X.head()
+    #le.inverse_transform(y)
 
-    # Mengambil kolom Rata-rata overdue dan mentranformasi menggunakan one-hot encoding
-    rata_overdue = pd.get_dummies(X["rata_rata_overdue"], prefix="overdue")
-    X = X.join(rata_overdue)
+    labels = pd.get_dummies(df.Potability).columns.values.tolist()
+    
+    "### Label"
+    labels
 
-    X = X.drop(columns="rata_rata_overdue")
+    # """## Normalisasi MinMax Scaler"""
 
-    labels = data["risk_rating"]
-    # 
-    KPR_status = pd.get_dummies(X["kpr_aktif"], prefix="KPR")
-    X = X.join(KPR_status)
-
-    # remove "rata_rata_overdue" feature
-    X = X.drop(columns = "kpr_aktif")
-
-    st.write("Menampilkan dataframe yang rata-rata overdue, risk rating dan kpr aktif sudah di drop")
-    st.dataframe(X)
-
-    st.write(" ## Normalisasi")
-    st.write("Normalize feature 'pendapatan_setahun_juta', 'durasi_pinjaman_bulan', 'jumlah_tanggungan'")
-    old_normalize_feature_labels = ['pendapatan_setahun_juta', 'durasi_pinjaman_bulan', 'jumlah_tanggungan']
-    new_normalized_feature_labels = ['norm_pendapatan_setahun_juta', 'norm_durasi_pinjaman_bulan', 'norm_jumlah_tanggungan']
-    normalize_feature = data[old_normalize_feature_labels]
-
-    st.dataframe(normalize_feature)
 
     scaler = MinMaxScaler()
+    scaler.fit(X)
+    X = scaler.transform(X)
+    X
 
-    scaler.fit(normalize_feature)
+    X.shape, y.shape
 
-    normalized_feature = scaler.transform(normalize_feature)
 
-    normalized_feature_df = pd.DataFrame(normalized_feature, columns = new_normalized_feature_labels)
 
-    st.write("Data setelah dinormalisasi")
-    st.dataframe(normalized_feature_df)
+with modelling:
+    X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=0.2,random_state=4)
+    from sklearn.preprocessing import StandardScaler
+    sc = StandardScaler()
+    X_train = sc.fit_transform(X_train)
+    X_test = sc.transform(X_test)
+    # from sklearn.feature_extraction.text import CountVectorizer
+    # cv = CountVectorizer()
+    # X_train = cv.fit_transform(X_train)
+    # X_test = cv.fit_transform(X_test)
+    st.write("""# Modeling """)
+    st.subheader("Berikut ini adalah pilihan untuk Modeling")
+    st.write("Pilih Model yang Anda inginkan untuk Cek Akurasi")
+    naive = st.checkbox('Naive Bayes')
+    kn = st.checkbox('K-Nearest Neighbor')
+    des = st.checkbox('Decision Tree')
+    mod = st.button("Modeling")
 
-    X = X.drop(columns = old_normalize_feature_labels)
+    # NB
+    GaussianNB(priors=None)
 
-    X = X.join(normalized_feature_df)
+    # Fitting Naive Bayes Classification to the Training set with linear kernel
+    nvklasifikasi = GaussianNB()
+    nvklasifikasi = nvklasifikasi.fit(X_train, y_train)
 
-    X = X.join(labels)
-
-    st.write("Dataframe X baru")
-    st.dataframe(X)
-
-    subject_lables = ["Unnamed: 0",  "kode_kontrak"]
-    X = X.drop(columns = subject_lables)
-
-    # percent_amount_of_test_data = / HUNDRED_PERCENT
-    percent_amount_of_test_data = 0.3
-
-    st.write("Dataframe X baru yang tidak ada fitur/kolom unnamed: 0 dan kode kontrak")
-    st.dataframe(X)
-    st.write("## Hitung Data")
-    st.write("- Pisahkan kolom risk rating dari data frame")
-    st.write("- Ambil kolom 'risk rating' sebagai target kolom untuk kategori kelas")
-    st.write("- Pisahkan data latih dengan data tes")
-    st.write("""            Spliting Data
-
-                data latih (nilai data)
-                X_train 
-
-                data tes (nilai data)
-                X_test 
-
-                data latih (kelas data)
-                y_train
-
-                data tes (kelas data)
-                y_test""")
-
-    # separate target 
-
-    # values
-    matrices_X = X.iloc[:,0:10].values
-
-    # classes
-    matrices_Y = X.iloc[:,10].values
-
-    X_1 = X.iloc[:,0:10].values
-    Y_1 = X.iloc[:, -1].values
-
-    # X_train, X_test, y_train, y_test = train_test_split(matrices_X, matrices_Y, test_size = percent_amount_of_test_data, random_state=0)
-    X_train, X_test, y_train, y_test = train_test_split(X_1, Y_1, test_size = percent_amount_of_test_data, random_state=0)
-
-    st.write("Menampilkan Y_1")
-    st.write(Y_1)
+    # Predicting the Test set results
+    y_pred = nvklasifikasi.predict(X_test)
     
-    st.write("Menampilkan X_1")
-    st.write(X_1)
-    ### Dictionary to store model and its accuracy
+    y_compare = np.vstack((y_test,y_pred)).T
+    nvklasifikasi.predict_proba(X_test)
+    akurasi = round(100 * accuracy_score(y_test, y_pred))
+    # akurasi = 10
 
-    model_accuracy = OrderedDict()
+    # KNN 
+    K=10
+    knn=KNeighborsClassifier(n_neighbors=K)
+    knn.fit(X_train,y_train)
+    y_pred=knn.predict(X_test)
 
-    ### Dictionary to store model and its precision
+    skor_akurasi = round(100 * accuracy_score(y_test,y_pred))
 
-    model_precision = OrderedDict()
+    # DT
 
-    ### Dictionary to store model and its recall
+    dt = DecisionTreeClassifier()
+    dt.fit(X_train, y_train)
+    # prediction
+    dt.score(X_test, y_test)
+    y_pred = dt.predict(X_test)
+    #Accuracy
+    akurasiii = round(100 * accuracy_score(y_test,y_pred))
 
-    model_recall = OrderedDict()
+    if naive :
+        if mod :
+            st.write('Model Naive Bayes accuracy score: {0:0.2f}'. format(akurasi))
+    if kn :
+        if mod:
+            st.write("Model KNN accuracy score : {0:0.2f}" . format(skor_akurasi))
+    if des :
+        if mod :
+            st.write("Model Decision Tree accuracy score : {0:0.2f}" . format(akurasiii))
+    
+    eval = st.button("Evaluasi semua model")
+    if eval :
+        # st.snow()
+        source = pd.DataFrame({
+            'Nilai Akurasi' : [akurasi,skor_akurasi,akurasiii],
+            'Nama Model' : ['Naive Bayes','KNN','Decision Tree']
+        })
+
+        bar_chart = alt.Chart(source).mark_bar().encode(
+            y = 'Nilai Akurasi',
+            x = 'Nama Model'
+        )
+
+        st.altair_chart(bar_chart,use_container_width=True)
+
+
+
+with implementation:
+    st.write("# Implementation")
+    ph = st.number_input('Masukkan pH Air')
+    Hardness = st.number_input('Masukkan Hardness Air')
+    Solids = st.number_input('Masukkan Solids Air')
+    Chloramines = st.number_input('Masukkan Chloramines Air')
+    Sulfate = st.number_input('Masukkan Sulfate Air')
+    Conductivity = st.number_input('Masukkan Conductivity Air')
+    Organic_carbon = st.number_input('Masukkan Organic Carbon Air')
+    Trihalomethanes = st.number_input('Masukkan Trihalomethanes Air')
+    Turbidity = st.number_input('Masukkan Turbidity Air')
+
+    def submit():
+        # input
+        inputs = np.array([[
+            ph,
+            Hardness,
+            Solids,
+            Chloramines,
+            Sulfate,
+            Conductivity,
+            Organic_carbon,
+            Trihalomethanes,
+            Turbidity
+            ]])
+        # st.write(inputs)
+        # baru = pd.DataFrame(inputs)
+        # input = pd.get_dummies(baru)
+        # st.write(input)
+        # inputan = np.array(input)
+        # import label encoder
+        le = joblib.load("le.save")
+        model1 = joblib.load("knn.joblib")
+        y_pred3 = model1.predict(inputs)
+        st.write(f"Berdasarkan data yang Anda masukkan, maka anda dinyatakan : {le.inverse_transform(y_pred3)[0]}")
+
+    all = st.button("Submit")
+    if all :
+        st.balloons()
+        submit()
+
