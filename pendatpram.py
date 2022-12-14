1f43091015bacfc3209911b94cf2844229c8303b
pramdf042 <84129088+pramdf042@users.noreply.github.com>
2022-12-14 22:21:09 +0700

    Update pendatpram.py

--- pendatpram.py
+++ pendatpram.py
@@ -1,164 +1,274 @@
 import streamlit as st
-import numpy as np
 import pandas as pd
+import numpy as np
+from PIL import Image
+import altair as alt
 from sklearn.preprocessing import MinMaxScaler
-from sklearn.preprocessing import StandardScaler
+from sklearn import preprocessing
 from sklearn.model_selection import train_test_split
-from collections import OrderedDict
+from sklearn.metrics import accuracy_score
 from sklearn.naive_bayes import GaussianNB
-from sklearn.metrics import confusion_matrix, accuracy_score, recall_score, precision_score, f1_score
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.tree import DecisionTreeClassifier
-from sklearn.ensemble import BaggingClassifier
-from sklearn.datasets import make_classification
-from sklearn.svm import SVC
-import os
-import warnings
-import altair as alt
-from sklearn.utils.validation import joblib
+# import warnings
+# warnings.filterwarnings("ignore")
 
 
-st.write(""" 
-# TUGAS PENDAT
-""")
+st.title("PENAMBANGAN DATA")
+st.write("##### Nama  : Pramudya Dwi Febrianto ")
+st.write("##### Nim   : 200411100042 ")
+st.write("##### Kelas : Penambangan Data C ")
 
-description, importdata, preprocessing, modelling, implementation = st.tabs(["Description", "Import Data", "Preprocessing", "Modelling", "Implementation"])
-warnings.filterwarnings("ignore")
-with importdata:
-    st.write("""# Upload File""")
-    uploaded_files = st.file_uploader("Upload file CSV", accept_multiple_files=True)
-    for uploaded_file in uploaded_files:
-        df = pd.read_csv(uploaded_file)
-        st.write("Nama File Anda = ", uploaded_file.name)
-        st.dataframe(df)
+data_set_description, upload_data, preprocessing, modeling, implementation = st.tabs(["Deskripsi Dataset", "Load Data", "Preprocessing", "Modeling", "Implementation"])
 
+with data_set_description:
+    st.write("""# Data Set Description """)
+    st.write("###### Data Set Ini Adalah : Klasifikasi Tipe Avocado) ")
+    st.write("###### Sumber Data Set dari Kaggle : https://www.kaggle.com/datasets/neuromusic/avocado-prices")
+    st.write("""###### Penjelasan setiap fitur : """)
+    st.write("""[1]. Avarage Price(Harga Rata-Rata) :
+
+    Kolom ini berisi data harga rata-rata satu buah alpukat
+    """)
+    st.write("""[2]. Total Volume :
+
+    Kolom ini berisi data jumlah total alpukat yang terjual
+    """)
+    st.write("""[3]. 4046 :
+
+    Kolom ini berisi data total jumlah alpukat dengan PLU 4046 terjual
+    """)
+    st.write("""[4]. 4225 :
+
+    Kolom ini berisi data total jumlah alpukat dengan PLU 4225 terjual
+    """)
+    st.write("""[5]. 4770 :
+
+    Kolom ini berisi data total jumlah alpukat dengan PLU 4770 terjual
+    """)
+    st.write("""[6]. Total Bags :
+
+    Kolom ini berisi data total jumlah dari small bags dan large bags dan xlarge bags
+    """)
+    st.write("""[7]. Small Bags :
+
+    Kolom ini berisi data total jumlah tas penjualan alpukat dengan ukuran kecil
+    """)
+    st.write("""[8]. Large Bags :
+
+    Kolom ini berisi data total jumlah tas penjualan alpukat dengan ukuran besar
+    """)
+    st.write("""[9]. XLarge Bags :
+
+    Kolom ini berisi data total jumlah tas penjualan alpukat dengan ukuran sangat besar
+    """)
+    st.write("""[10]. Type :
+
+    Kolom ini berisi output yang menunjukkan tipe alpukat
+    """)
+   
+    st.write("###### Source Code Aplikasi ada di Github anda bisa acces di link : https://github.com/pramdf042/pendatpram")
+    st.write("###### Untuk kontak bisa menghubungi nomor wa berikut '081249150396 ")
+
+with upload_data:
+    # uploaded_files = st.file_uploader("Upload file CSV", accept_multiple_files=True)
+    # for uploaded_file in uploaded_files:
+    #     df = pd.read_csv(uploaded_file)
+    #     st.write("Nama File Anda = ", uploaded_file.name)
+    #     st.dataframe(df)
+    df = pd.read_csv('https://raw.githubusercontent.com/pramdf042/datamining/main/avocado.csv')
+    st.dataframe(df)
 with preprocessing:
-    prepros = st.radio(
-    "Silahkan pilih metode preprocessing :",
-    ("Normalisasi", "Min Max Scaler", "Categorical to Numeric"))
-    prepoc = st.button("Preprocessing")
-
-    if prepros == "Min Max Scaler":
-        if prepoc:
-            df[["Area", "MajorAxisLength", "MinorAxisLength", "Eccentricity", "ConvexArea", "EquivDiameter", "Extent", "Perimeter", "Roundness", "AspectRation"]].agg(['min','max'])
-            df.Class.value_counts()
-            X = df.drop(columns=["Class","id"])
-            y = df["Class"]
-            "### Normalize data transformasi"
-            X
-            X.shape, y.shape
-            #   le.inverse_transform(y)
-            labels = pd.get_dummies(df.Class).columns.values.tolist()
-            "### Label"
-            labels
-            # """## Normalisasi MinMax Scaler"""
-            scaler = MinMaxScaler()
-            scaler.fit(X)
-            X = scaler.transform(X)
-            X
-            X.shape, y.shape
-
-
-
-
-with modelling:
-    # df = pd.read_csv("https://raw.githubusercontent.com/pramdf042/datamining/main/riceClassification.csv")
-    X=df.iloc[:,:-1]
-    y=df.iloc[:,-1]
-    X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=0.2,random_state=4)
-    from sklearn.preprocessing import StandardScaler
-    sc = StandardScaler()
-    X_train = sc.fit_transform(X_train)
-    X_test = sc.transform(X_test)
-    # from sklearn.feature_extraction.text import CountVectorizer
-    # cv = CountVectorizer()
-    # X_train = cv.fit_transform(X_train)
-    # X_test = cv.fit_transform(X_test)
-    st.write("""# Modeling """)
-    st.subheader("Berikut ini adalah pilihan untuk Modeling")
-    st.write("Pilih Model yang Anda inginkan untuk Cek Akurasi")
-    naive = st.checkbox('Naive Bayes')
-    kn = st.checkbox('K-Nearest Neighbor')
-    des = st.checkbox('Decision Tree')
-    mod = st.button("Modeling")
-
-    # NB
-    GaussianNB(priors=None)
-
-    # Fitting Naive Bayes Classification to the Training set with linear kernel
-    nvklasifikasi = GaussianNB()
-    nvklasifikasi = nvklasifikasi.fit(X_train, y_train)
-
-    # Predicting the Test set results
-    y_pred = nvklasifikasi.predict(X_test)
+    st.subheader("""Normalisasi Data""")
+    st.write("""Rumus Normalisasi Data :""")
+    st.image('https://i.stack.imgur.com/EuitP.png', use_column_width=False, width=250)
+    st.markdown("""
+    Dimana :
+    - X = data yang akan dinormalisasi atau data asli
+    - min = nilai minimum semua data asli
+    - max = nilai maksimum semua data asli
+    """)
+    #Mendefinisikan Varible X dan Y
     
-    y_compare = np.vstack((y_test,y_pred)).T
-    nvklasifikasi.predict_proba(X_test)
-    akurasi = round(100 * accuracy_score(y_test, y_pred))
-    # akurasi = 10
-
-    # KNN 
-    K=10
-    knn=KNeighborsClassifier(n_neighbors=K)
-    knn.fit(X_train,y_train)
-    y_pred=knn.predict(X_test)
-
-    skor_akurasi = round(100 * accuracy_score(y_test,y_pred))
-
-    # DT
-
-    dt = DecisionTreeClassifier()
-    dt.fit(X_train, y_train)
-    # prediction
-    dt.score(X_test, y_test)
-    y_pred = dt.predict(X_test)
-    #Accuracy
-    akurasiii = round(100 * accuracy_score(y_test,y_pred))
-
-    if naive :
-        if mod :
-            st.write('Model Naive Bayes accuracy score: {0:0.2f}'. format(akurasi))
-    if kn :
-        if mod:
-            st.write("Model KNN accuracy score : {0:0.2f}" . format(skor_akurasi))
-    if des :
-        if mod :
-            st.write("Model Decision Tree accuracy score : {0:0.2f}" . format(akurasiii))
+    X = df.drop(columns=["Unnamed: 0","Date","year", "region", "type"],axis=1)
+    y = df['type'].values
+    df
+    X
+    df_min = X.min()
+    df_max = X.max()
     
+    #NORMALISASI NILAI X
+    scaler = MinMaxScaler()
+    #scaler.fit(features)
+    #scaler.transform(features)
+    scaled = scaler.fit_transform(X)
+    features_names = X.columns.copy()
+    #features_names.remove('label')
+    scaled_features = pd.DataFrame(scaled, columns=features_names)
 
+    st.subheader('Hasil Normalisasi Data')
+    st.write(scaled_features)
+
+    st.subheader('Target Label')
+    dumies = pd.get_dummies(df.type).columns.values.tolist()
+    dumies = np.array(dumies)
+
+    labels = pd.get_dummies(df.type).columns.values.tolist()
+
+    st.write(labels)
+
+    # st.subheader("""Normalisasi Data""")
+    # st.write("""Rumus Normalisasi Data :""")
+    # st.image('https://i.stack.imgur.com/EuitP.png', use_column_width=False, width=250)
+    # st.markdown("""
+    # Dimana :
+    # - X = data yang akan dinormalisasi atau data asli
+    # - min = nilai minimum semua data asli
+    # - max = nilai maksimum semua data asli
+    # """)
+    # df.weather.value_counts()
+    # df = df.drop(columns=["date"])
+    # #Mendefinisikan Varible X dan Y
+    # X = df.drop(columns=['weather'])
+    # y = df['weather'].values
+    # df_min = X.min()
+    # df_max = X.max()
+
+    # #NORMALISASI NILAI X
+    # scaler = MinMaxScaler()
+    # #scaler.fit(features)
+    # #scaler.transform(features)
+    # scaled = scaler.fit_transform(X)
+    # features_names = X.columns.copy()
+    # #features_names.remove('label')
+    # scaled_features = pd.DataFrame(scaled, columns=features_names)
+
+    # #Save model normalisasi
+    # from sklearn.utils.validation import joblib
+    # norm = "normalisasi.save"
+    # joblib.dump(scaled_features, norm) 
+
+
+    # st.subheader('Hasil Normalisasi Data')
+    # st.write(scaled_features)
+
+with modeling:
+    training, test = train_test_split(scaled_features,test_size=0.2, random_state=1)#Nilai X training dan Nilai X testing
+    training_label, test_label = train_test_split(y, test_size=0.2, random_state=1)#Nilai Y training dan Nilai Y testing
+    with st.form("modeling"):
+        st.subheader('Modeling')
+        st.write("Pilihlah model yang akan dilakukan pengecekkan akurasi:")
+        naive = st.checkbox('Gaussian Naive Bayes')
+        k_nn = st.checkbox('K-Nearest Neighboor')
+        destree = st.checkbox('Decission Tree')
+        submitted = st.form_submit_button("Submit")
+
+        # NB
+        GaussianNB(priors=None)
+
+        # Fitting Naive Bayes Classification to the Training set with linear kernel
+        gaussian = GaussianNB()
+        gaussian = gaussian.fit(training, training_label)
+
+        # Predicting the Test set results
+        y_pred = gaussian.predict(test)
+    
+        y_compare = np.vstack((test_label,y_pred)).T
+        gaussian.predict_proba(test)
+        gaussian_akurasi = round(100 * accuracy_score(test_label, y_pred))
+        # akurasi = 10
+
+        #Gaussian Naive Bayes
+        # gaussian = GaussianNB()
+        # gaussian = gaussian.fit(training, training_label)
+
+        # probas = gaussian.predict_proba(test)
+        # probas = probas[:,1]
+        # probas = probas.round()
+
+        # gaussian_akurasi = round(100 * accuracy_score(test_label,probas))
+
+        #KNN
+        K=10
+        knn=KNeighborsClassifier(n_neighbors=K)
+        knn.fit(training,training_label)
+        knn_predict=knn.predict(test)
+
+        knn_akurasi = round(100 * accuracy_score(test_label,knn_predict))
+
+        #Decission Tree
+        dt = DecisionTreeClassifier()
+        dt.fit(training, training_label)
+        # prediction
+        dt_pred = dt.predict(test)
+        #Accuracy
+        dt_akurasi = round(100 * accuracy_score(test_label,dt_pred))
+
+        if submitted :
+            if naive :
+                st.write('Model Naive Bayes accuracy score: {0:0.2f}'. format(gaussian_akurasi))
+            if k_nn :
+                st.write("Model KNN accuracy score : {0:0.2f}" . format(knn_akurasi))
+            if destree :
+                st.write("Model Decision Tree accuracy score : {0:0.2f}" . format(dt_akurasi))
+        
+        grafik = st.form_submit_button("Grafik akurasi semua model")
+        if grafik:
+            data = pd.DataFrame({
+                'Akurasi' : [gaussian_akurasi, knn_akurasi, dt_akurasi],
+                'Model' : ['Gaussian Naive Bayes', 'K-NN', 'Decission Tree'],
+            })
+
+            chart = (
+                alt.Chart(data)
+                .mark_bar()
+                .encode(
+                    alt.X("Akurasi"),
+                    alt.Y("Model"),
+                    alt.Color("Akurasi"),
+                    alt.Tooltip(["Akurasi", "Model"]),
+                )
+                .interactive()
+            )
+            st.altair_chart(chart,use_container_width=True)
+  
 with implementation:
-    st.write("# Implementation")
-    Area = st.number_input('Masukkan Area')
-    MajorAxisLength = st.number_input('Masukkan MajorAxisLength')
-    MinorAxisLength = st.number_input('Masukkan MinorAxisLength')
-    Eccentricity = st.number_input('Masukkan Eccentricity')
-    ConvexArea = st.number_input('Masukkan ConvexArea')
-    EquivDiameter = st.number_input('Masukkan EquivDiameter')
-    Extent = st.number_input('Masukkan Extent')
-    Perimeter = st.number_input('Masukkan Perimeter')
-    Roundness = st.number_input('Masukkan Roundness')
-    AspectRation = st.number_input('Masukkan AspectRation')
+    with st.form("my_form"):
+        st.subheader("Implementasi")
+        AveragePrice = st.number_input('Masukkan Average Price')
+        TotalVolume = st.number_input('Masukkan Total Volume')
+        label4046 = st.number_input('Masukkan 4046')
+        label4225 = st.number_input('Masukkan 4225')
+        label4770 = st.number_input('Masukkan 4770')
+        TotalBags = st.number_input('Masukkan Total Bags')
+        SmallBags = st.number_input('Masukkan Small Bags')
+        LargeBags = st.number_input('Masukkan Large Bags')
+        XLargeBags = st.number_input('Masukkan XLarge Bags')
+        model = st.selectbox('Pilihlah model yang akan anda gunakan untuk melakukan prediksi?',
+                ('Gaussian Naive Bayes', 'K-NN', 'Decision Tree'))
 
-    def submit():
-        # input
-        inputs = np.array([[
-            Area, MajorAxisLength, MinorAxisLength, Eccentricity, ConvexArea, EquivDiameter, Extent, Perimeter, Roundness, AspectRation
-        ]])
-        # st.write(inputs)
-        # baru = pd.DataFrame(inputs)
-        # input = pd.get_dummies(baru)
-        # st.write(input)
-        # inputan = np.array(input)
-        # import label encoder
-        le = joblib.load("le.save")
-        model1 = joblib.load("tree.joblib")
-        y_pred3 = model1.predict(inputs)
-        if le.inverse_transform(y_pred3)[0]==1:
-            hasilakhir='Jasmine'
-        else :
-            hasilakhir='Gonen'
-        st.write(f"Berdasarkan data yang Anda masukkan, maka beras dinyatakan memiliki jenis: {hasilakhir}")
+        prediksi = st.form_submit_button("Submit")
+        if prediksi:
+            inputs = np.array([
+            AveragePrice, TotalVolume, label4046, label4225, label4770, TotalBags, SmallBags, LargeBags, XLargeBags
+            ])
 
-    all = st.button("Submit")
-    if all :
-        submit()
+            df_min = X.min()
+            df_max = X.max()
+            input_norm = ((inputs - df_min) / (df_max - df_min))
+            input_norm = np.array(input_norm).reshape(1, -1)
+
+            if model == 'Gaussian Naive Bayes':
+                mod = gaussian
+            if model == 'K-NN':
+                mod = knn 
+            if model == 'Decision Tree':
+                mod = dt
+
+            input_pred = mod.predict(input_norm)
+
+
+            st.subheader('Hasil Prediksi')
+            st.write('Menggunakan Pemodelan :', model)
+
+            st.write(input_pred)
